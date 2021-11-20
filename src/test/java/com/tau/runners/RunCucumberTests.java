3c50a193a96f112748e2854f412b51fc1f9aece5
Dan <d.dziuba@softwizard.pl>
2021-11-20 12:48:57 +0100

    *adding CustomerCarePage *adding ParaSoftDemoWebsitePage *adding WelcomeToParaBankPage

---
+++ src/test/java/com/tau/runners/RunCucumberTests.java
@@ -0,0 +1,17 @@
+package com.tau.runners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(glue={"com.tau.steps"},
+		features = "src/test/resources",
+		//dryRun = true, // pokaze metody ktore musi zaimplementowac // gdy chce uruchomic testy muze zakomentoac ta linijke
+		plugin = { "pretty", "html:target/site/cucumber-pretty",
+		"json:target/cucumber.json" })
+public class RunCucumberTests {
+
+
+}
